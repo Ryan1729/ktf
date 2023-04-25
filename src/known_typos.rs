5ebd5266833ffd7f0f9a7755cb1bea8a4bd329fa
Ryan1729 <Ryan1729@gmail.com>
2023-04-25 01:06:00 -0600

    add `ignore` and `grep` and print out matches

---
+++ src/known_typos.rs
@@ -0,0 +1,14 @@
+
+const LENGTH: usize = 3;
+
+pub const TYPOS: [&str; LENGTH] = [
+    "don;t",
+    "won;t",
+    "can;t",
+];
+
+pub const FIXES: [&str; LENGTH] = [
+    "don't",
+    "won't",
+    "can't",
+];
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use grep::{
+    matcher::Matcher,
+    regex::RegexMatcherBuilder,
+    searcher::{sinks::UTF8, Searcher}
+};
+
+use ignore::WalkBuilder;
+
+mod known_typos;
+use known_typos::{FIXES, TYPOS};
+
 fn main() {
-    todo!("ktf");
+    let mut searcher = Searcher::new();
+    let matcher = RegexMatcherBuilder::new()
+        .build_literals(&TYPOS)
+        .expect("TYPOS should produce a valid matcher");
+
+    let mut builder = WalkBuilder::new("./");
+        builder
+        .add_custom_ignore_filename(".ktfignore")
+        .skip_stdout(true);
+    for result in builder.build() {
+        let entry = match result {
+            Ok(entry) => entry,
+            Err(err) => {
+                eprintln!("ERROR: {}", err);
+                continue
+            },
+        };
+
+        let path = entry.path();
+
+        searcher.search_path(&matcher, &path, UTF8(|lnum, line| {
+            let r#match = matcher.find(line.as_bytes());
+            println!("{}:{lnum} {} {:?}", path.display(), line, r#match);
+
+            Ok(true)
+        }));
+    }
 }
